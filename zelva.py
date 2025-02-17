8e3d45d6d8874522f7af3c103c005283c6a062cc
CaptainChristoph <kiki.ruchar@gmail.com>
2025-02-17 12:31:32 +0100

    jj

---
+++ zelva.py
@@ -0,0 +1,22 @@
+from turtle import forward, left, right, exitonclick, shape
+
+shape('turtle')
+
+left(90)
+forward(100)
+right(90)
+forward(100)
+right(135)
+forward(142)
+left(135)
+forward(100)
+left(90)
+forward(100)
+left(45)
+forward(70)
+left(90)
+forward(70)
+left(90)
+forward(142)
+
+exitonclick()
